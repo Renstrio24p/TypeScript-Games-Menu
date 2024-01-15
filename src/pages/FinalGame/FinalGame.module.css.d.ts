316d906f251e44184222e1b682e909ab9f5ceda0
Renstrio24p <Renstrio24p@users.noreply.github.com>
2024-01-15 09:01:10 +0000

    deploy: 547d61188d54a8d703f89397e9efa8025fbb0c27

---
+++ src/pages/FinalGame/FinalGame.module.css.d.ts
@@ -0,0 +1,12 @@
+export declare const canvas: string;
+export declare const container: string;
+export declare const layer: string;
+export declare const player: string;
+
+declare const ClassNames: {
+  "canvas": string;
+  "container": string;
+  "layer": string;
+  "player": string;
+};
+export default ClassNames;
